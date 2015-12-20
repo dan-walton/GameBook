5a4a6a31a2ef1a633b8d89989a9bfb1fe23a07de
dan-walton <danielwalton07@gmail.com>
2015-12-20 15:56:12 +1000

    Update game with new events, fix code

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,16 +1,11 @@
-function newGame() = {
-
-// Alert and prompt for user name
-
-	alert("The year is 2034. Algorithm-writing programming machines, now accountable for over 75% of the trades on the Dow Jones Index, have replaced many of the world's richest companies' stock portfolios with wool futures, causing the second global financial crisis. Having just been elected to office, you are faced with an economic crisis and must deal with business, consumers and the unions to bring the country back to prosperity.");
-	var userName = prompt("Please enter your name");
+function newGame() {
 
 // Define approval rating - affects public perception & reaction
 
 	var approvalRating = {
-		value: 50;
-		add: function(approval) {
-			switch(approval){
+		value: 50,
+		add: function(result) {
+			switch(result){
 				case "success": this.value += 10; break;
 				case "failure" : this.value -= 10; break;
 			}
@@ -20,9 +15,10 @@ function newGame() = {
 // Define negotiating ability - affects ability to negotiate
 
 	var negotiatingAbility = {
-		value: 50;
+		value: 50,
 		add: function(result) {
 			switch(result){
+				case "major success": this.value += 20; break;
 				case "success": this.value += 10; break;
 				case "failure": this.value -= 10; break;
 			}
@@ -32,8 +28,8 @@ function newGame() = {
 // Define health of economy - win if greater than 50 - lose otherwise
 
 	var economyHealth = {
-		value: 20;
-		add: function(economy) {
+		value: 20,
+		add: function(result) {
 			switch(result){
 				case "success" : this.value += 10; break;
 				case "failure": this.value -=10; break;
@@ -41,19 +37,22 @@ function newGame() = {
 		}
 	}
 
+	function finalScore() {
+		return economyHealth.value + negotiatingAbility.value + approvalRating.value;
+	}
+
 // Finds political ideology of player character
 
 	function findIdeology() {
-		var userIdeology = prompt("Please enter your ideology: Left, Right or Centre").toLowerCase();
 		if (userIdeology ==="right") {
 			alert("For business!");
 			firstEvent();
 		} 
-		else if (userAnswer === "left") {
+		else if (userIdeology === "left") {
 			alert("For labour!");
 			firstEvent();
 		}
-		else if (userAnswer === "centre") {
+		else if (userIdeology === "centre") {
 			alert("Middle of the road!");
 			firstEvent();
 		}
@@ -66,8 +65,7 @@ function newGame() = {
 // First event - passing of budget
 
 	function firstEvent() {
-		var userReaction = prompt("During your first day in parliament, you motion an emergency budget: opposed heavily by the opposition who block the motion in the senate.
-		Do you negotiate, concede or relent to the opposition.").toLowerCase;
+		var userReaction = prompt("During your first day in parliament, you motion an emergency budget: opposed heavily by the opposition who block the motion in the senate. Do you negotiate, concede or relent to the opposition.").toLowerCase();
 		if (userReaction == "concede") {
 			alert("You concede defeat to the opposition and fail to pass the budget.")
 			negotiatingAbility.add("failure");
@@ -76,7 +74,7 @@ function newGame() = {
 		}
 		else if (userReaction == "negotiate") {
 			var successRate = Math.random();
-			if (successRate >= 0.5)
+			if (successRate >= 0.5) {
 				if (userIdeology == "right") {
 					alert("You negotiate with the opposition, dropping your austerity measures to pass tax cuts.");
 				}
@@ -89,13 +87,14 @@ function newGame() = {
 				negotiatingAbility.add("success");
 				economyHealth.add("success");
 				approvalRating.add("success");
+			}
 			else {
 				alert("You fail to pass a budget and the economy worsens");
 				negotiatingAbility.add("failure");
 				economyHealth.add("failure");
 				approvalRating.add("failure");
 			}
-			secondEvent();
+		secondEvent();
 		}
 		else if (userReaction == "relent") {
 			var successRate = Math.random();
@@ -130,58 +129,165 @@ function newGame() = {
 	}
 
 	function secondEvent() {
-		if (economyHealth >= 30) {
-			var userChoice = prompt("After passing the budget through parliament, the economy has begun to pick up, but you need some help to further alleviate unemployment levels. 
-			Do you approach the unions or business to give a kickstart to workers in the economy?").toLowerCase;
+		if (economyHealth.value > 25) {
+			var userChoice = prompt("After passing the budget through parliament, the economy has begun to pick up, but you need some help to further alleviate unemployment levels. Do you approach labour or business to give a kickstart to workers in the economy?").toLowerCase();
 		}
 		else {
-			var userChoice = prompt("After the whole budget debacle, the economy remains stagnant and needs a kickstart to alleviate the worsening unemployment level.
-			Do you approach the unions or business to do so?").toLowerCase;
+			var userChoice = prompt("After the whole budget debacle, the economy remains stagnant and needs a kickstart to alleviate the worsening unemployment level. Do you approach labour or business to do so?").toLowerCase();
 		}
-
+		
 		if (userChoice == "business") {
-			if (userIdeology == "right")
+			if (userIdeology == "right") {
 				alert("You stick with your conservative ideology and approach JackWalt Corp for economic aid.");
-				alert(jackWalt.randomAction);
+				alert(jackWalt.actions.invest);
+				economyHealth.add("success");
+				approvalRating.add("success");
+				negotiatingAbility.add("success");
+				alert("Through adept negotiation, you have managed to get business on your side and improve the economy. Well done!")
 			}
 
 			else {
 				alert("An interesting choice given your political leanings. You approach the famous JackWalt Corp for economic aid.")
-				alert(jackWalt.randomAction);
+				if (negotiatingAbility.value >= 60) {
+					alert(jackWalt.actions.invest);
+					economyHealth.add("success");
+					approvalRating.add("success");
+					negotiatingAbility.add("major success");
+					alert("Through adept negotiation, you have managed to get business on your side and improve the economy. Well done!");
+				}
+
+				else {
+					alert(jackWalt.actions.layoff);
+					economyHealth.add("failure");
+					approvalRating.add("failure");
+					negotiatingAbility.add("failure");
+					alert("You have failed to get this group on your side at cost to the economy and your popularity");
+				}
 			}
 		}
-		else if (userChoice == "unions") {
-			if (userIdeology == "left")
+		
+		else if (userChoice == "labour") {
+			if (userIdeology == "left") {
 				alert("You stick with your liberal ideology and approach the Workers Equality Union for economic aid.");
-				alert(unions.randomAction);
+				alert(unions.actions.help);
+				economyHealth.add("success");
+				approvalRating.add("success");
+				negotiatingAbility.add("success");
+				alert("Through adept negotiation, you have managed to get the unions on your side and improve the economy. Well done!");
 			}
 
 			else {
-				alert("An interesting choice given your political leanings. You approach the Workers Equality Union for economic aid.")
-				alert(unions.randomAction);
+				alert("An interesting choice given your political leanings. You approach the famous JackWalt Corp for economic aid.")
+				if (negotiatingAbility.value >= 60) {
+					alert(unions.actions.help);
+					economyHealth.add("success");
+					approvalRating.add("success");
+					negotiatingAbility.add("major success");
+					alert("Through adept negotiation, you have managed to get the unions on your side and improve the economy. Well done!");
+				}
+
+				else {
+					alert(unions.actions.strike);
+					economyHealth.add("failure");
+					approvalRating.add("failure");
+					negotiatingAbility.add("failure");
+					alert("You have failed to get this group on your side at cost to the economy and your popularity");
+				}
 			}
 		}
+
 		else {
-			alert("Please enter either business or unions");
+			alert("Please enter either labour or business.")
 			secondEvent();
 		}
 
-		thirdEvent();
+		thirdEvent();		
 	}
 
 	function thirdEvent() {
-		// Consumer action based on approval rating
+		if (approvalRating.value >= 60) {
+			alert("After a succcessful first term, you have been re-elected on a dramatic margin. Consumers are rejoiced with the news and decide to go on a spending spree. \nEconomy +10!");
+			economyHealth.add("success");
+		}
+		else if (approvalRating.value <= 40) {
+			alert("After a mismanaged term in parliament, your party has been voted out and the economy remains in shambles.");
+			return;
+		}
+		else {
+			var coup = prompt("After a mediocre first term, your party has been re-elected on the slightest of margins. However, your deputy leader calls for a leadership spill. Do you stand down or remain?").toLowerCase;
+			if (coup == "stand down") {
+				alert("You stand down from leadership, and the economy falls apart.")
+				return;
+			}
+			else if (coup == "remain") {
+				var coupOdds = Math.random();
+				if (coupOdds >= 0.33) {
+					alert("You remain in parliament after toppling your opponent in the leadership spill.");
+					negotiatingAbility.add("success");
+				}
+				else {
+					alert("BREAKING NEWS: " + userName + " has been removed from leadership in a close spill. The stockmarket has fallen 1000points on the news.")
+					return;
+				}
+			}
+		}
+
+		fourthEvent();
 	}
 
 	function fourthEvent() {
-		// If/else statement creates two paths based on health of the economy
+		var count = 0;
+		while (count < 4) {
+			var budget = prompt("It is time to deliver your yearly budget. Your policy advisors have put forth six ideas to improve the economy's health: raise taxes, lower taxes, increase spending, decrease spending, increase regulation, decrease regulation. Which do you choose?").toLowerCase();
+			switch(budget) {
+				case "raise taxes": alert("You raise taxes, to the detriment of both the economy and your popularity."); economyHealth.add("failure"); approvalRating.add("failure"); break;
+				case "lower taxes": alert("You lower taxes, boosting consumer spending and your own popularity."); economyHealth.add("success"); approvalRating.add("success"); break;
+				case "increase spending": alert("You increase spending, creating jobs and improving your popularity."); economyHealth.add("success"); approvalRating.add("success"); break;
+				case "decrease spending": alert("You decrease spending, met with outrage by consumers"); economyHealth.add("failure"); approvalRating.add("failure"); break;
+				case "increase regulation": alert("You increase regulation, stifling business."); economyHealth.add("failure"); break;
+				case "decreae regulation": alert("You decrease regulation, improving business confidence"); economyHealth.add("success"); break;
+				default: alert("try again")
+			}
+			count += 1
+		}
+
+		if (approvalRating <= 25) {
+			alert("Your archrival xxx_donaldtrump_xxx has announced a spill and defeated you, ending your political career. GAME OVER");
+			return;
+		}
+
+		else {
+			alert("Well done. You have made it to the end of your two terms through cunning and wit. Let's see how you've done.")
+		}
+		
+		fifthEvent();
 	}
 
 	function fifthEvent() {
-		// Determines whether the player has won or not
+		if (economyHealth >= 50 && voterApproval >= 50) {
+			alert("Well done, you have successfully maintained your approval rating whilst boosting the economy. Your final score is: " + finalScore());
+			return;
+		}
+
+		else {
+			alert("You have managed to scrape through the past eight years, always on your toes. Your final score is: " + finalScore());
+			return;
+		}
+
 	}
 	
 	// Code to get the functions going
+
+	// Alert and prompt for user name
+
+	
+	alert("The year is 2034. Algorithm-writing programming machines, now accountable for over 75% of the trades on the Dow Jones Index, have replaced many of the world's richest companies' stock portfolios with wool futures, causing the second global financial crisis. Having just been elected to office, you are faced with an economic crisis and must deal with business, consumers and the unions to bring the country back to prosperity.");
+	var userName = prompt("Please enter your name");
+	do {
+		var userIdeology = prompt("Please enter your ideology: Left, Right or Centre").toLowerCase();
+	}
+	while (userIdeology !== "left" && userIdeology !== "right" && userIdeology !== "centre");
+
 	findIdeology();
 
 };
